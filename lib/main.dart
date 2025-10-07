1ca3e5b8980e1b036eb32b111c9357837b0e3f5a
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-06 23:00:58 -0300

    Add TodoApp Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/screens/home_screen.dart';
 
 void main() {
-  runApp(const HomeScreen());
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
 }
