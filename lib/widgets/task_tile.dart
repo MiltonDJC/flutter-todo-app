d62484c90e79380da25aa12a46b1e77331037f1d
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-07 21:49:16 -0300

    Develop task tile widget

---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  final String title;
+  final bool isCompleted;
+  final ValueChanged<bool?> onChanged;
+
+  const TaskTile({
+    super.key,
+    required this.title,
+    required this.isCompleted,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(
+          decoration: isCompleted ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Checkbox(value: isCompleted, onChanged: onChanged),
+    );
+  }
+}
