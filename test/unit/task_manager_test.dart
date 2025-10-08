82ab224b0e9c89f57bcfc3709aa65fb8cf298094
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-08 20:36:36 -0300

    Add tests for task_manager

---
+++ test/unit/task_manager_test.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo_app/managers/task_manager.dart';
+
+void main() {
+  late TaskManager taskManager;
+
+  group('TaskManager', () {
+    setUp(() {
+      taskManager = TaskManager();
+    });
+
+    test(
+      'Given the task manager list, when a task is added, then the task manager list increases by one.',
+      () {
+        final int tasksLength = taskManager.tasks.length;
+        taskManager.addTask('Learn Flutter');
+        expect(taskManager.tasks.length, tasksLength + 1);
+      },
+    );
+    test(
+      "Given the task manager list, when a task is added, then title of the task is 'Learn Flutter'.",
+      () {
+        taskManager.addTask('Learn Flutter');
+        expect(
+          taskManager.tasks[taskManager.tasks.length - 1].title,
+          'Learn Flutter',
+        );
+      },
+    );
+    test(
+      "Given the task manager list, when an empty task is try to be added, then the task should not be available to be added.",
+      () {
+        final tasksLength = taskManager.tasks.length;
+        taskManager.addTask('');
+        expect(taskManager.tasks.length, equals(tasksLength));
+      },
+    );
+    test(
+      "Given the task manager list, when a task with spaces is try to be added, then the task should not be available to be added.",
+      () {
+        final tasksLength = taskManager.tasks.length;
+        taskManager.addTask('   ');
+        expect(taskManager.tasks.length, equals(tasksLength));
+      },
+    );
+  });
+
+  group('toggleTask', () {
+    setUp(() {
+      taskManager = TaskManager();
+    });
+    test(
+      'Given a manager with one incomplete task, when toggleTask() is called, then task becomes completed',
+      () {
+        taskManager.addTask('Learn Flutter');
+        taskManager.toggleTask(taskManager.tasks[0]);
+        expect(taskManager.tasks[0].isCompleted, isTrue);
+      },
+    );
+
+    test(
+      'Given a manager with one completed task, when toggleTask() is called, then task becomes incomplete',
+      () {
+        taskManager.addTask('Learn Flutter');
+        taskManager.tasks[0].isCompleted = true;
+        taskManager.toggleTask(taskManager.tasks[0]);
+        expect(taskManager.tasks[0].isCompleted, isFalse);
+      },
+    );
+  });
+}
