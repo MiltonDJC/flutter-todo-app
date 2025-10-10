05dddb7356e951c375da5b9fb3fc8c3c49bd2db5
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-10 10:16:52 -0300

    Add method to delete task and update method to add task

--- lib/managers/task_manager.dart
+++ lib/managers/task_manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_todo_app/models/task.dart';
+import 'package:flutter_todo_app/utils/task_in_list.dart';
 
 class TaskManager {
   TaskManager();
@@ -9,7 +10,15 @@ class TaskManager {
 
   void addTask(String title) {
     if (title.trim().isEmpty) return;
-    _tasks.add(Task(title));
+    if (!taskInList(title, tasks)) {
+      _tasks.add(Task(title));
+    }
+  }
+
+  void deleteTask(String title) {
+    if (taskInList(title, tasks)) {
+      _tasks.removeWhere((task) => task.title == title);
+    }
   }
 
   void toggleTask(Task task) {
