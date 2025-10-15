8e688ecc2b408e4db4a2b1858eec901a173abd64
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-15 19:15:01 -0300

    Add Task Detail Screen

---
+++ lib/screens/task_detail_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/models/task.dart';
+
+class TaskDetailScreen extends StatelessWidget {
+  const TaskDetailScreen({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: Text(task.title),
+      ),
+      body: Center(
+        child: Text(
+          task.isCompleted ? 'Tarea Completada' : 'Tarea Incompleta',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+      ),
+    );
+  }
+}
