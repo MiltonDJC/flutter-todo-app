70a4fe02f91ee5dd944332e0f99dccc1e19d82cc
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-16 09:14:54 -0300

    Refactor home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -31,9 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
             // Change between theme modes.
             Switch(
               value: widget.isDarkMode,
-              onChanged: (_) {
-                widget.onToggleTheme();
-              },
+              onChanged: (_) => widget.onToggleTheme(),
             ),
           ],
         ),
@@ -44,73 +42,56 @@ class _HomeScreenState extends State<HomeScreen> {
             return TaskTile(
               title: task.title,
               isCompleted: task.isCompleted,
-              onChanged: (_) {
-                setState(() {
-                  taskManager.toggleTask(task);
-                });
-              },
-              onPressed: () {
-                setState(() {
-                  taskManager.deleteTask(task.title);
-                });
-              },
+              onChanged: (_) => setState(() => taskManager.toggleTask(task)),
+              onPressed: () =>
+                  setState(() => taskManager.deleteTask(task.title)),
               // Navigate to task detail.
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TaskDetailScreen(task: task),
-                  ),
-                );
-              },
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TaskDetailScreen(task: task),
+                ),
+              ),
             );
           },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                String newTaskTitle = '';
-                return AlertDialog(
-                  title: const Text('Nueva tarea'),
-                  content: TextField(
-                    key: const Key('taskTextField'),
-                    autofocus: true,
-                    onChanged: (value) {
-                      newTaskTitle = value;
-                    },
+          onPressed: () => showDialog(
+            context: context,
+            builder: (context) {
+              String newTaskTitle = '';
+              return AlertDialog(
+                title: const Text('Nueva tarea'),
+                content: TextField(
+                  key: const Key('taskTextField'),
+                  autofocus: true,
+                  onChanged: (value) => newTaskTitle = value,
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancelar'),
                   ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
+                  TextButton(
+                    onPressed: () {
+                      if (newTaskTitle.trim().isNotEmpty) {
+                        String capitalizedText =
+                            newTaskTitle.substring(0, 1).toUpperCase() +
+                            newTaskTitle.substring(1);
+                        setState(() => taskManager.addTask(capitalizedText));
                         Navigator.pop(context);
-                      },
-                      child: const Text('Cancelar'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        if (newTaskTitle.trim().isNotEmpty) {
-                          String capitalizedText =
-                              newTaskTitle.substring(0, 1).toUpperCase() +
-                              newTaskTitle.substring(1);
-                          setState(() {
-                            taskManager.addTask(capitalizedText);
-                          });
-                          Navigator.pop(context);
-                        } else {
-                          Fluttertoast.showToast(
-                            msg: 'Escriba el nombre de la tarea',
-                          );
-                        }
-                      },
-                      child: const Text('Agregar'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
+                      } else {
+                        Fluttertoast.showToast(
+                          msg: 'Escriba el nombre de la tarea',
+                        );
+                      }
+                    },
+                    child: const Text('Agregar'),
+                  ),
+                ],
+              );
+            },
+          ),
           child: const Icon(Icons.add),
         ),
       ),
